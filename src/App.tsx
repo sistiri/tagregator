d8802fc3500c82cc07b5d9d42e9179b0ac587d94
sistiri <sistiri@yahoo.com>
2022-03-09 12:40:54 +0100

    addNewUrlHandler(), Bookmark Class

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+
 import NewUrl from "./components/NewUrl";
+import { Bookmark } from "./models/bookmark.model";
 
 import "./App.css";
 
-function App() {
-  const addNewUrlHandler = (newUrlInput: string) => {
-    console.log(newUrlInput);
+const App: React.FC = () => {
+  const [myBookmarks, setMyBookmarks] = useState<Bookmark[]>([]);
+
+  const addNewUrlHandler = (url: string) => {
+    if (myBookmarks.some((bm) => bm.url === url)) {
+      return;
+    }
+    const newBookmark = new Bookmark(url);
+    setMyBookmarks((prevBookmarks) => [...prevBookmarks, newBookmark]);
   };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <NewUrl onAddUrl={addNewUrlHandler}></NewUrl>
-      </header>
+      <NewUrl onAddUrl={addNewUrlHandler}></NewUrl>
+      <ul>
+        {myBookmarks.map((bm) => (
+          <li key={bm.id}>{bm.url}</li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/models/bookmark.model.tsx
@@ -0,0 +1,14 @@
+export class Bookmark {
+    id: string;
+    url: string;
+    date: Date;
+    tags?: string[];
+    snapshot?: string;
+    comments?: Comment[];
+
+    constructor(url: string) {
+        this.id = Math.random().toString()
+        this.url = url
+        this.date = new Date()
+    }
+}
