4b5b66f5fb6bf54dfa0588aff4cc9ecee6d14474
sistiri <sistiri@yahoo.com>
2022-03-23 12:10:46 +0100

    Refactoring New Bookmark Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 import Layout from "./components/layout/Layout";
-import NewUrl from "./components/bookmarks/NewUrl";
+import NewBookmark from "./components/bookmarks/NewBookmark";
 import { Bookmark } from "./models/bookmark.model";
 import useHttp from "./hooks/use-http";
 
@@ -11,14 +11,9 @@ import Bookmarks from "./components/bookmarks/Bookmarks";
 const App: React.FC = () => {
   const [myBookmarks, setMyBookmarks] = useState<Bookmark[]>([]);
 
-  const {
-    isLoading,
-    error,
-    sendRequest: fetchBookmarks,
-  } = useHttp();
+  const { isLoading, error, sendRequest: fetchBookmarks } = useHttp();
 
   useEffect(() => {
-
     const transformBookmarks = (bookmarksObj: { [id: string]: Bookmark }) => {
       const loadedBookmarks = [];
       for (const key in bookmarksObj) {
@@ -36,42 +31,20 @@ const App: React.FC = () => {
       // }
     };
 
-    fetchBookmarks({
-      url: "https://tagregatory-default-rtdb.europe-west1.firebasedatabase.app/bookmarks.json",
-    }, transformBookmarks);
+    fetchBookmarks(
+      {
+        url: "https://tagregatory-default-rtdb.europe-west1.firebasedatabase.app/bookmarks.json",
+      },
+      transformBookmarks
+    );
   }, [fetchBookmarks]);
 
-  const addNewUrlHandler = (url: string) => {
-    if (myBookmarks.some((bm) => bm.url === url) || url.length < 1) {
+  const addBookmarkHandler = (bookmark: Bookmark) => {
+    if (myBookmarks.some((bm) => bm.url === bookmark.url)) {
+      console.log("URL already exists!");
       return;
     }
-
-    const newBookmarkBase = {
-      url: url,
-      date: new Date(),
-    };
-    fetch(
-      "https://tagregatory-default-rtdb.europe-west1.firebasedatabase.app/bookmarks.json",
-      {
-        method: "POST",
-        body: JSON.stringify(newBookmarkBase),
-        headers: { "Content-Type": "application/json" },
-      }
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((responseData) => {
-        const newBookmark = new Bookmark(url);
-        setMyBookmarks((prevBookmarks) => [
-          ...prevBookmarks,
-          { ...newBookmark, ...{ id: responseData.name } },
-        ]);
-      });
-    // localStorage.setItem(
-    //   "myBookmarks",
-    //   JSON.stringify([...myBookmarks, newBookmark])
-    // );
+    setMyBookmarks((prevBookmarks) => prevBookmarks.concat(bookmark));
   };
 
   const removeBookmarkHandler = (id: string) => {
@@ -84,17 +57,19 @@ const App: React.FC = () => {
         headers: { "Content-Type": "application/json" },
       }
     );
-    // localStorage.setItem("myBookmarks", JSON.stringify(filteredBookmarks));
   };
 
   const addTagsHandler = (id: string, enteredTags: string[]) => {
     const bookmarkIndexToEdit = myBookmarks.findIndex((bm) => bm.id === id);
     const newMyBookmarks = [...myBookmarks];
-    const allTags = [
-      ...newMyBookmarks[bookmarkIndexToEdit].tags,
-      ...enteredTags,
-    ];
-    newMyBookmarks[bookmarkIndexToEdit].tags = allTags;
+    let allTags: string[];
+    if (!myBookmarks[bookmarkIndexToEdit].tags) {
+      allTags = enteredTags;
+    } else {
+      allTags = [...newMyBookmarks[bookmarkIndexToEdit].tags, ...enteredTags];
+      newMyBookmarks[bookmarkIndexToEdit].tags = allTags;
+    }
+
     console.log(newMyBookmarks[bookmarkIndexToEdit].tags);
     setMyBookmarks(newMyBookmarks);
     fetch(
@@ -110,7 +85,10 @@ const App: React.FC = () => {
   return (
     <Layout>
       <div className="App">
-        <NewUrl onAddUrl={addNewUrlHandler} />
+        <NewBookmark
+          onAddBookmark={addBookmarkHandler}
+          myBookmarks={myBookmarks}
+        />
         <Bookmarks
           bookmarks={myBookmarks}
           loading={isLoading}
---
+++ src/components/bookmarks/NewBookmark.tsx
@@ -0,0 +1,66 @@
+import React, { Fragment } from "react";
+import useHttp from "../../hooks/use-http";
+
+import Card from "../UI/Card";
+import { Bookmark } from "../../models/bookmark.model";
+import BookmarkForm from "./BookmarkForm";
+
+import classes from "./NewBookmark.module.css";
+
+type NewBookmarkProps = {
+  myBookmarks : Bookmark[] ;
+  onAddBookmark: (createdBookmark: Bookmark) => void;
+};
+
+const NewBookmark: React.FC<NewBookmarkProps> = (props) => {
+  const { isLoading, error, sendRequest: postBookmarkRequest } = useHttp();
+
+  const createBookmark = (
+    newBookmarkBase: { url: string; date: Date, tags: string[] },
+    responseData: { name: string }
+  ) => {
+    const createdBookmark: Bookmark = {
+      ...newBookmarkBase,
+      ...{ id: responseData.name },
+    };
+    console.log(newBookmarkBase);
+    console.log(createdBookmark);
+    props.onAddBookmark(createdBookmark);
+  };
+
+  const enterUrlHandler = async (enteredUrl: string) => {
+
+    if(props.myBookmarks.some(bm => bm.url === enteredUrl)){
+      console.log('You have already saved this URL!')
+      return;
+    }
+
+    const newBookmarkBase = {
+      url: enteredUrl,
+      date: new Date(),
+      tags: []
+    };
+
+    postBookmarkRequest(
+      {
+        url: "https://tagregatory-default-rtdb.europe-west1.firebasedatabase.app/bookmarks.json",
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: newBookmarkBase,
+      },
+      createBookmark.bind(null, newBookmarkBase)
+    );
+    
+  };
+
+  return (
+    <Card className={classes.card}>
+      <Fragment>
+        <BookmarkForm onEnterUrl={enterUrlHandler} loading={isLoading} />
+        {error && <p>{error}</p>}
+      </Fragment>
+    </Card>
+  );
+};
+
+export default NewBookmark;
