384844f511172941f739aac6b706a62f501a7f75
sistiri <sistiri@yahoo.com>
2022-03-23 12:10:02 +0100

    Refactoring Tags Component

--- src/components/bookmarks/Tags.tsx
+++ src/components/bookmarks/Tags.tsx
@@ -1,18 +1,32 @@
 import { Fragment } from "react";
+import Button from "../UI/Button";
 import classes from "./Tags.module.css";
 
 type TagsProps = {
   tags: string[];
+  onShowAddTags: () => void;
   filteredBy?: string;
   filterPhrase?: string;
 };
 
 const Tags: React.FC<TagsProps> = (props) => {
   return (
-    <Fragment >
-      {props.tags.map((t: string) => (
-        <span key={t} className={classes.tag}>{t}</span>
-      ))}
+    <Fragment>
+      {props.tags ? (
+        props.tags.map((t: string) => (
+          <span key={t} className={classes.tag}>
+            {t}
+          </span>
+        ))
+      ) : (
+        <Button
+          className={classes.button}
+          type="button"
+          onClick={props.onShowAddTags}
+        >
+          Add some tags!
+        </Button>
+      )}
     </Fragment>
   );
 };
