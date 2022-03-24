ec43ccece0432965dca1fde49db298452bf62ecf
sistiri <sistiri@yahoo.com>
2022-03-24 11:07:13 +0100

    Creating MyBookmarks Page Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,104 +1,13 @@
-import React, { useEffect, useState } from "react";
-
+import React from "react";
 import Layout from "./components/layout/Layout";
-import NewBookmark from "./components/bookmarks/NewBookmark";
-import { Bookmark } from "./models/bookmark.model";
-import useHttp from "./hooks/use-http";
-
+import MyBookmarks from "./components/pages/MyBookmarks";
 import "./App.css";
-import Bookmarks from "./components/bookmarks/Bookmarks";
 
 const App: React.FC = () => {
-  const [myBookmarks, setMyBookmarks] = useState<Bookmark[]>([]);
-
-  const { isLoading, error, sendRequest: fetchBookmarks } = useHttp();
-
-  useEffect(() => {
-    const transformBookmarks = (bookmarksObj: { [id: string]: Bookmark }) => {
-      const loadedBookmarks = [];
-      for (const key in bookmarksObj) {
-        loadedBookmarks.push({
-          id: key,
-          url: bookmarksObj[key].url,
-          date: bookmarksObj[key].date,
-          tags: bookmarksObj[key].tags,
-          snapshot: bookmarksObj[key].snapshot,
-          comments: bookmarksObj[key].comments,
-        });
-      }
-      // if (loadedBookmarks !== null) {
-      setMyBookmarks(loadedBookmarks);
-      // }
-    };
-
-    fetchBookmarks(
-      {
-        url: "https://tagregatory-default-rtdb.europe-west1.firebasedatabase.app/bookmarks.json",
-      },
-      transformBookmarks
-    );
-  }, [fetchBookmarks]);
-
-  const addBookmarkHandler = (bookmark: Bookmark) => {
-    if (myBookmarks.some((bm) => bm.url === bookmark.url)) {
-      console.log("URL already exists!");
-      return;
-    }
-    setMyBookmarks((prevBookmarks) => prevBookmarks.concat(bookmark));
-  };
-
-  const removeBookmarkHandler = (id: string) => {
-    const filteredBookmarks = myBookmarks.filter((bm) => bm.id !== id);
-    setMyBookmarks(filteredBookmarks);
-    fetch(
-      `https://tagregatory-default-rtdb.europe-west1.firebasedatabase.app/bookmarks/${id}.json`,
-      {
-        method: "DELETE",
-        headers: { "Content-Type": "application/json" },
-      }
-    );
-  };
-
-  const addTagsHandler = (id: string, enteredTags: string[]) => {
-    const bookmarkIndexToEdit = myBookmarks.findIndex((bm) => bm.id === id);
-    const newMyBookmarks = [...myBookmarks];
-    let allTags: string[];
-    if (!myBookmarks[bookmarkIndexToEdit].tags) {
-      allTags = enteredTags;
-    } else {
-      allTags = [...newMyBookmarks[bookmarkIndexToEdit].tags!, ...enteredTags];
-    }
-    newMyBookmarks[bookmarkIndexToEdit].tags = allTags;
-
-    console.log(newMyBookmarks[bookmarkIndexToEdit].tags);
-    setMyBookmarks(prevBookMarks => prevBookMarks=newMyBookmarks);
-    fetch(
-      `https://tagregatory-default-rtdb.europe-west1.firebasedatabase.app/bookmarks/${id}.json`,
-      {
-        method: "PATCH",
-        body: JSON.stringify({ tags: allTags }),
-        headers: { "Content-Type": "application/json" },
-      }
-      );
-      console.log(newMyBookmarks)
-      
-  };
-
   return (
     <Layout>
       <div className="App">
-        <NewBookmark
-          onAddBookmark={addBookmarkHandler}
-          myBookmarks={myBookmarks}
-        />
-        <Bookmarks
-          bookmarks={myBookmarks}
-          loading={isLoading}
-          error={error}
-          onFetch={fetchBookmarks}
-          onRemoveBookmark={removeBookmarkHandler}
-          onAddTags={addTagsHandler}
-        />
+        <MyBookmarks />
       </div>
     </Layout>
   );
--- src/components/pages/MyBookmarks.tsx
+++ src/components/pages/MyBookmarks.tsx
@@ -1,14 +1,107 @@
-import React, { Fragment } from 'react'
+import React, { useEffect, useState } from "react";
 
-type MyBookmarksProps = {}
+import Layout from "../layout/Layout";
+import NewBookmark from "../bookmarks/NewBookmark";
+import { Bookmark } from "../../models/bookmark.model";
+import useHttp from "../../hooks/use-http";
 
-const MyBookmarks = (props: MyBookmarksProps) => {
+import classes from "./MyBookmarks.module.css";
+import Bookmarks from "../bookmarks/Bookmarks";
+
+const MyBookmarks: React.FC = () => {
+  const [myBookmarks, setMyBookmarks] = useState<Bookmark[]>([]);
+
+  const { isLoading, error, sendRequest: fetchBookmarks } = useHttp();
+
+  useEffect(() => {
+    const transformBookmarks = (bookmarksObj: { [id: string]: Bookmark }) => {
+      const loadedBookmarks = [];
+      for (const key in bookmarksObj) {
+        loadedBookmarks.push({
+          id: key,
+          url: bookmarksObj[key].url,
+          date: bookmarksObj[key].date,
+          tags: bookmarksObj[key].tags,
+          snapshot: bookmarksObj[key].snapshot,
+          comments: bookmarksObj[key].comments,
+        });
+      }
+      // if (loadedBookmarks !== null) {
+      setMyBookmarks(loadedBookmarks);
+      // }
+    };
+
+    fetchBookmarks(
+      {
+        url: "https://tagregatory-default-rtdb.europe-west1.firebasedatabase.app/bookmarks.json",
+      },
+      transformBookmarks
+    );
+  }, [fetchBookmarks]);
+
+  const addBookmarkHandler = (bookmark: Bookmark) => {
+    if (myBookmarks.some((bm) => bm.url === bookmark.url)) {
+      console.log("URL already exists!");
+      return;
+    }
+    setMyBookmarks((prevBookmarks) => prevBookmarks.concat(bookmark));
+  };
+
+  const removeBookmarkHandler = (id: string) => {
+    const filteredBookmarks = myBookmarks.filter((bm) => bm.id !== id);
+    setMyBookmarks(filteredBookmarks);
+    fetch(
+      `https://tagregatory-default-rtdb.europe-west1.firebasedatabase.app/bookmarks/${id}.json`,
+      {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  };
+
+  const addTagsHandler = (id: string, enteredTags: string[]) => {
+    const bookmarkIndexToEdit = myBookmarks.findIndex((bm) => bm.id === id);
+    const newMyBookmarks = [...myBookmarks];
+    let allTags: string[];
+    if (!myBookmarks[bookmarkIndexToEdit].tags) {
+      allTags = enteredTags;
+    } else {
+      allTags = [...newMyBookmarks[bookmarkIndexToEdit].tags!, ...enteredTags];
+    }
+    newMyBookmarks[bookmarkIndexToEdit].tags = allTags;
+
+    console.log(newMyBookmarks[bookmarkIndexToEdit].tags);
+    setMyBookmarks(prevBookMarks => prevBookMarks=newMyBookmarks);
+    fetch(
+      `https://tagregatory-default-rtdb.europe-west1.firebasedatabase.app/bookmarks/${id}.json`,
+      {
+        method: "PATCH",
+        body: JSON.stringify({ tags: allTags }),
+        headers: { "Content-Type": "application/json" },
+      }
+      );
+      console.log(newMyBookmarks)
+      
+  };
+
   return (
-    <Fragment>
-        MyBookmarks
+    <Layout>
+      <div className="MyBookmarks">
+        <NewBookmark
+          onAddBookmark={addBookmarkHandler}
+          myBookmarks={myBookmarks}
+        />
+        <Bookmarks
+          bookmarks={myBookmarks}
+          loading={isLoading}
+          error={error}
+          onFetch={fetchBookmarks}
+          onRemoveBookmark={removeBookmarkHandler}
+          onAddTags={addTagsHandler}
+        />
+      </div>
+    </Layout>
+  );
+};
 
-    </Fragment>
-  )
-}
-
-export default MyBookmarks
+export default MyBookmarks;
